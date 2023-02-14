c411d9e913fcbf855bbf92ff72cbca61299ff59c
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-02-14 16:54:41 +0200

    add Title to FormCard

---
+++ src/Component/FormTitle/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DivTitle } from "./style";
+
+const FormTitle = ({ title }) => {
+  return (
+    <div>
+      <DivTitle>{title}</DivTitle>
+    </div>
+  );
+};
+
+export default FormTitle;
