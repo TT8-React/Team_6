c8594ce430856204852bd5594e2710b83900b183
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-02-14 16:10:28 +0200

    add style to formCard

---
+++ src/Component/FormCard/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const DivFormCard = styled.div`
+  background: #f8f8f8;
+  border: 1px solid #000000;
+  border-radius: 30px;
+  width: 522px;
+  padding: 65px 83px 32px 79px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
