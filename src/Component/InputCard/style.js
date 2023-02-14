10b96036b6aec72c624175494ba9601dfa97745a
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-02-14 16:58:19 +0200

    add style to Input Card

---
+++ src/Component/InputCard/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const DivInput = styled.div`
+  margin-bottom: 25px;
+`;
+
+export const Label = styled.label`
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 150%;
+  color: #52525c;
+  margin-top: 25px;
+`;
+export const Input = styled.input`
+  margin-top: 8px;
+  width: 360px;
+  height: 48px;
+  background: #ffffff;
+  border: 2px solid #d9d9d9;
+  border-radius: 8px;
+  outline: none;
+  padding-left: 10px;
+`;
