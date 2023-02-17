13780c1c3ea3262f51c107d56e1ce6e2f68a3589
mohammed zakout <mohammedzakkh@gmail.com>
2023-02-17 21:08:13 +0300

    adding data for detail cart

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,17 +1,25 @@
-
-
 export const detailReport = [
-    { id: 1, detail: "Corporate Identity Number (CIN) of the Company" },
-    { id: 2, detail: "Name of the comapny" },
-    { id: 3, detail: "Year of incorporation" },
-    { id: 4, detail: "Registered office address	" },
-    { id: 5, detail: "Corporate address" },
-    { id: 6, detail: "Email" },
-    { id: 7, detail: "Telephone" },
-    { id: 8, detail: "Website" },
-    { id: 9, detail: "Financial year for reporting is being done" },
-    { id: 10, detail: "Whether shares listed on recognized Stock Exchange(s):" },
-    { id: 11, detail: "Authorised Capital	" },
-    { id: 12, detail: "Paid Up Capital" },
-  ];
-  
+  { id: 1, detail: "Corporate Identity Number (CIN) of the Company" },
+  { id: 2, detail: "Name of the comapny" },
+  { id: 3, detail: "Year of incorporation" },
+  { id: 4, detail: "Registered office address	" },
+  { id: 5, detail: "Corporate address" },
+  { id: 6, detail: "Email" },
+  { id: 7, detail: "Telephone" },
+  { id: 8, detail: "Website" },
+  { id: 9, detail: "Financial year for reporting is being done" },
+  { id: 10, detail: "Whether shares listed on recognized Stock Exchange(s):" },
+  { id: 11, detail: "Authorised Capital	" },
+  { id: 12, detail: "Paid Up Capital" },
+];
+
+export const detailCardData = [
+  { title: "Name", text: "Lorem ipsum" },
+
+  { title: "test1", text: "Lorem ipsum" },
+  { title: "test2", text: "Lorem ipsum" },
+  { title: "test3", text: "Lorem ipsum" },
+  { title: "test4", text: "Lorem ipsum" },
+  { title: "test5", text: "Lorem ipsum" },
+  { title: "test6", text: "Lorem ipsum" },
+];
