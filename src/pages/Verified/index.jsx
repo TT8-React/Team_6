4c4fec62deecb30f240950b33ccf1db9969fecdc
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-02-17 23:43:45 +0200

    add VerifiedPage

---
+++ src/pages/Verified/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import FormCard from "../../components/FormCard";
+import FormTitle from "../../components/FormTitle";
+
+import succsess from "../../assets/photos/succsess.png";
+import NavButton from "../../components/NavButton";
+
+import styled from "styled-components";
+
+import { Flex } from "../../global/style";
+
+const Image = styled.img`
+  width: 184px;
+  height: 184px;
+  margin-top: 59px;
+`;
+
+const DivNavButton = styled.div`
+  margin-top: 83px;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 70px;
+`;
+
+const Verified = () => {
+  return (
+    <div>
+      <FormCard>
+        <FormTitle title="Verified" />
+        <Flex justify="center">
+          <Image src={succsess} alt="succsess" />
+        </Flex>
+        <DivNavButton>
+          <NavButton name="NEXT" />
+        </DivNavButton>
+      </FormCard>
+    </div>
+  );
+};
+
+export default Verified;
