ceacb961408c69333846331f739ae405c82d6a8a
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-02-17 17:48:52 +0200

    modify on FormCard and add a FormQues

--- src/components/FormCard/index.jsx
+++ src/components/FormCard/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormQuestion from "../FormQuestion";
 import FormTitle from "../FormTitle";
 import InputCard from "../InputCard";
 import { DivFormCard } from "./style";
@@ -9,6 +10,7 @@ const FormCard = () => {
       <FormTitle title="LOG IN" />
       <InputCard label="Email ID" disc="Enter Email ID" typeInput="email" />
       <InputCard label="Password" disc="Enter password" typeInput="Password" />
+      <FormQuestion Ques="Forgotpassword ?" />
     </DivFormCard>
   );
 };
