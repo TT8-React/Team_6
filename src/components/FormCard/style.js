8da9de642ea3a719b081e30feb308ef89d21244d
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2023-02-17 18:15:55 +0200

    add style to DivButton

--- src/components/FormCard/style.js
+++ src/components/FormCard/style.js
@@ -11,3 +11,8 @@ export const DivFormCard = styled.div`
   top: 50%;
   transform: translate(-50%, -50%);
 `;
+export const DivButton = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 53px;
+`;
