b9795fc04e1a6faff53a89967e23a1b3c597222e
Ashraf shawwa <ashraf_shawa@hotmail.com>
2023-02-18 18:41:33 +0200

    Full layout component  sidebar  header and content

--- src/components/LayOut/index.jsx
+++ src/components/LayOut/index.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "../Header";
 import SideBar from "../SideBar";
 
 const LayOutBox = styled.div`
   height: 100vh;
+  width: 100vw;
   display: flex;
 `;
 
-export default function LayOut(children) {
+const RightBox = styled.div`
+  width: calc(100% - 270px);
+`;
+
+export default function LayOut(props) {
   return (
     <LayOutBox>
       <SideBar />
+      <RightBox>
+        <Header title="Dashboard" />
+        <div> here will put the children {props.children} </div>
+      </RightBox>
     </LayOutBox>
   );
 }
