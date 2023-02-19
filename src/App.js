9ac15c84a06a2cdbab6066523cc037b9ef7f72a0
mohammed zakout <mohammedzakkh@gmail.com>
2023-02-19 14:57:47 +0300

    adding routes for forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { GlobalStyle } from "./global/style";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/index";
+import SignUp from "./pages/SignUp/index";
+import ForgotPass from './pages/ForgotPass/index';
+import Verification from './pages/Verification/index';
+import Verified from './pages/Verified/index';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <GlobalStyle />
+    <div>
+      <Routes>
+        <Route index element={<Login />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgotpassword" element={<ForgotPass />} />
+        <Route path="/verification" element={<Verification />} />
+        <Route path="/verified" element={<Verified />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
