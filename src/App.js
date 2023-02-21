4420add68c42be848d5794ba2e0a997db7eb9e3e
mohammed zakout <mohammedzakkh@gmail.com>
2023-02-21 11:17:51 +0300

    finish the work and deploy the project

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React, { useEffect } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
 
-//pages
-import Login from "./pages/Login/index";
-import SignUp from "./pages/SignUp/index";
-import ForgotPass from "./pages/ForgotPass/index";
-import Verification from "./pages/Verification/index";
-import Verified from "./pages/Verified/index";
-import Dashboard from "./pages/Dashboard/index";
-import Environment from "./pages/Environment/index";
-import Governance from "./pages/Governance/index";
-import Social from "./pages/Social/index";
-import Report from "./pages/Report/index";
-import GriReport from "./pages/GriReport/index";
-import SebiReport from "./pages/SebiReport/index";
-import CustomizeReport from "./pages/CustomizeReport/index";
-import ProfileDetails from "./pages/ProfileDetails/index";
-
-import { useAuthContext } from "./contexts/AuthContext";
-import CompanyDetails from "./pages/CompanyDetails";
-import Tabs from "./pages/Tabs/index";
-import MyReport from "./pages/MyReport/index";
-import CreateReport from "./pages/CreateReport/index";
 import { useAdminContext } from "./contexts/AdminContext";
+import Routers from "./routers";
 
 const App = () => {
-  const { isAuthorized, setisAuthorized } = useAuthContext();
   const { setDetails, setReports } = useAdminContext();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setisAuthorized(true);
-    }
-
     const savedDetails = JSON.parse(localStorage.getItem("savedDetails"));
 
     if (savedDetails) {
@@ -45,89 +18,11 @@ const App = () => {
     if (savedReports) {
       setReports(savedReports);
     }
-  }, [setisAuthorized, setDetails, setReports]);
+  }, [setDetails, setReports]);
 
-  return (
-    <div>
-      <Routes>
-        <Route index element={isAuthorized ? <Verified /> : <Login />} />
-        <Route
-          path="/login"
-          element={isAuthorized ? <Navigate to="/verified" /> : <Login />}
-        />
-        <Route
-          path="/signup"
-          element={isAuthorized ? <Navigate to="/verified" /> : <SignUp />}
-        />
-        <Route
-          path="/forgotpassword"
-          element={isAuthorized ? <ForgotPass /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/verification"
-          element={isAuthorized ? <Verification /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/verified"
-          element={isAuthorized ? <Verified /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/dashboard"
-          element={isAuthorized ? <Dashboard /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/environment"
-          element={isAuthorized ? <Environment /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/governance"
-          element={isAuthorized ? <Governance /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/social"
-          element={isAuthorized ? <Social /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/report"
-          element={isAuthorized ? <Report /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/griReport"
-          element={isAuthorized ? <GriReport /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/sebiReport"
-          element={isAuthorized ? <SebiReport /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/customizeReport"
-          element={
-            isAuthorized ? <CustomizeReport /> : <Navigate to="/Login" />
-          }
-        />
-        <Route
-          path="/companyDetails"
-          element={isAuthorized ? <CompanyDetails /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/tabs"
-          element={isAuthorized ? <Tabs /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/myReport"
-          element={isAuthorized ? <MyReport /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/profileDetails"
-          element={isAuthorized ? <ProfileDetails /> : <Navigate to="/Login" />}
-        />
-        <Route
-          path="/createReport"
-          element={isAuthorized ? <CreateReport /> : <Navigate to="/Login" />}
-        />
-      </Routes>
-    </div>
-  );
+  return <div>
+    <Routers />
+  </div>;
 };
 
 export default App;
